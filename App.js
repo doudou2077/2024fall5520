2e5e6dfd48490f073926cc74e58c7c5cbe1a257a
doudou <dushuying1996@gmail.com>
2024-09-10 15:00:25 -0700

    added a input component and moved to text component in it

--- App.js
+++ App.js
@@ -1,27 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
-import Header from './Components/Header';
 import React, { useState } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View } from 'react-native';
+import Header from './Components/Header';
+import Input from './Components/Input';
 
 export default function App() {
   const [text, setText] = useState("");
   const appName = "My App";
+
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <Header name={appName} />
-      <TextInput
-        // placeholder ="Type here"
-        autoCorrect={true}
-        keyboardType='default'
-        value={text}
-        style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
-        onChangeText={function (changeText) {
-          console.log(changedText);
-          setText(changeText);
-        }
-        }
-      />
+      <Input setText={setText} />
       <Text>{text}</Text>
     </View>
   );
