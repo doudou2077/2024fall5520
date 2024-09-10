d57329b33f3099cff446bf332119ba20e643599b
doudou <dushuying1996@gmail.com>
2024-09-10 14:48:50 -0700

    added textinput component and added text component to show the text that user enters in the textinput

--- App.js
+++ App.js
@@ -1,13 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 import Header from './Components/Header';
+import React, { useState } from 'react';
 
 export default function App() {
-  const { appName } = "My App";
+  const [text, setText] = useState("");
+  const appName = "My App";
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <Header name={appName} />
+      <TextInput
+        // placeholder ="Type here"
+        autoCorrect={true}
+        keyboardType='default'
+        value={text}
+        style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+        onChangeText={function (changeText) {
+          console.log(changedText);
+          setText(changeText);
+        }
+        }
+      />
+      <Text>{text}</Text>
     </View>
   );
 }
