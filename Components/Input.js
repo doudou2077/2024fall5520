84606538bc64358bcb757de09c7fc3f26e05b4f7
doudou <dushuying1996@gmail.com>
2024-09-10 19:36:19 -0700

    update the Input component to accept a shouldFocus prop that controls the autoFocus attribute of the TextInput

--- Components/Input.js
+++ Components/Input.js
@@ -1,19 +1,20 @@
-import { TextInput } from 'react-native';
 import React, { useState } from 'react';
+import { TextInput } from 'react-native';
 
-export default function Input() {
+export default function Input({ shouldFocus }) {
     const [text, setText] = useState("");
     return (
         <TextInput
             placeholder="Type here"
-            autoCorrect={true}
+            autoCorrect={false}
             keyboardType='default'
             value={text}
-            style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+            style={{ height: 40, borderColor: 'gray', borderWidth: 1, width: '80%' }}
             onChangeText={(changedText) => {
                 console.log(changedText);
                 setText(changedText);
             }}
+            autoFocus={shouldFocus}
         />
     );
 }
