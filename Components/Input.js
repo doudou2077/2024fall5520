7264d1b3a096cdb3a9c6f4fb2606303dfaf854be
doudou <dushuying1996@gmail.com>
2024-09-10 19:44:29 -0700

    add view component to wrap textinput and also for the purpose of adding text component later

--- Components/Input.js
+++ Components/Input.js
@@ -1,20 +1,23 @@
 import React, { useState } from 'react';
-import { TextInput } from 'react-native';
+import { TextInput, View } from 'react-native';
 
 export default function Input({ shouldFocus }) {
     const [text, setText] = useState("");
     return (
-        <TextInput
-            placeholder="Type here"
-            autoCorrect={false}
-            keyboardType='default'
-            value={text}
-            style={{ height: 40, borderColor: 'gray', borderWidth: 1, width: '80%' }}
-            onChangeText={(changedText) => {
-                console.log(changedText);
-                setText(changedText);
-            }}
-            autoFocus={shouldFocus}
-        />
+        <View>
+            <TextInput
+                placeholder="Type here"
+                autoCorrect={false}
+                keyboardType='default'
+                value={text}
+                style={{ height: 40, borderColor: 'gray', borderWidth: 1, width: '80%' }}
+                onChangeText={(changedText) => {
+                    console.log(changedText);
+                    setText(changedText);
+                }}
+                autoFocus={shouldFocus}
+            />
+
+        </View>
     );
 }
