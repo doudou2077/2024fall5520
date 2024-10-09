586831ff434ca6c9e0f025f8d5f95e72035bdcce
doudou <dushuying1996@gmail.com>
2024-10-08 21:13:39 -0700

    pass the onPressIn and onPressOut to the pressablebutton component

--- Components/PressableButton.js
+++ Components/PressableButton.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Pressable, Text, StyleSheet } from 'react-native';
 
-const PressableButton = ({ children, onPress, style, pressedStyle, textStyle }) => {
+const PressableButton = ({ children, onPress, style, pressedStyle, textStyle, onPressIn, onPressOut }) => {
     return (
         <Pressable
             onPress={onPress}
+            onPressIn={onPressIn}
+            onPressOut={onPressOut}
             style={({ pressed }) => [
                 styles.button,
                 style,
